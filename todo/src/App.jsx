2e6eb67419a6ea4cd1e24d550e1e3c5d64964a65
sachin4449 <sachin.panghal@bookmiseat.com>
2025-08-26 16:13:01 +0530

    project initailized

---
+++ todo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
